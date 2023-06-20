03dfa8300f82dcdb5de0e05f89ebebb2a910b070
Tae's PC <53635845+taeyeong2k@users.noreply.github.com>
2023-06-20 18:02:26 +1000

    add game page.tsx

---
+++ app/game/page.tsx
@@ -0,0 +1,51 @@
+'use client';
+import Phaser from 'phaser';
+import { useEffect } from 'react';
+
+const GameScene = new Phaser.Class({
+  Extends: Phaser.Scene,
+
+  initialize: function() {
+    Phaser.Scene.call(this, { key: 'gameScene' });
+  },
+
+  preload: function (){
+    // Preload assets
+  },
+
+  create: function() {
+    // Create game entities
+  },
+
+  update: function() {
+    // Update game entities
+  }
+});
+
+
+const gameConfig: Phaser.Types.Core.GameConfig = {
+    title: 'Arcane Barrage',
+    type: Phaser.AUTO,
+    scale: {
+      width: window.innerWidth,
+      height: window.innerHeight,
+    },
+    scene: GameScene,
+    parent: 'game-container',
+  };
+
+const gamePage = () => {
+  useEffect(() => {
+    if (document.getElementById('game-container')?.childNodes.length === 0) {
+      new Phaser.Game(gameConfig);
+    }
+    return () => {};
+  }, []);
+  
+  return (
+    <div id="game-container" />
+    );
+  
+  };
+
+  export default gamePage;
